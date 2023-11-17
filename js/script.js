ac8e43cd570c00fc343c25176b51ac356dea8604
pietrostraneo <straneo_p@yahoo.it>
2023-11-17 13:04:29 +0100

    Complete

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-let number = 100
-console.debug(number)
+let number = 100;
+console.debug(number);
+
+const PI = 3.14;
+// PI = 3.1416; ERRORE
+console.log(PI);
+
+let radius = 8;
+let circle = radius * 2 * PI;
+console.log(circle);
+
+let name = 'Carlo';
+console.log(name);
+
+name = 'Marco';
+console.log(name);
